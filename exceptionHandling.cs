f9a4ad20edb3d6c8b40a21c85742d9e0debe4b0a
AdamVignesh <69408077+AdamVignesh@users.noreply.github.com>
2023-03-26 23:19:40 +0530

    exception handling, user defined exception

---
+++ exceptionHandling.cs
@@ -0,0 +1,55 @@
+﻿namespace exceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23;
+            int b = 0;
+
+            try
+            {
+                int c = a / b;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("you cannot try to divide by 0");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception caught " + e);
+            }
+            finally
+            {
+                Console.WriteLine("Whatever happens......finally executes");
+            }
+            try
+            {
+                checkAge(14);
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+            public static void checkAge(int age)
+            {
+                if (age < 18)
+                {
+                    throw new Exception("you should be above 18 to vote");
+                }
+
+            }
+            public class myException : Exception
+            {
+                public myException(string message) : base(message)
+                {
+
+                }
+            }
+    
+    }
+    
+    
+}
