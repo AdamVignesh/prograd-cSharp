94e6dbaec31804ccc19585509a9bee265e97312c
AdamVignesh <69408077+AdamVignesh@users.noreply.github.com>
2023-03-28 22:14:48 +0530

    Lambda expression

---
+++ lambda.cs
@@ -0,0 +1,24 @@
+﻿namespace LambdaExpression
+{
+   // delegate void add(int a, int b);
+    delegate void welcome(string msg);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //add d1 = (num1,num2) =>Console.WriteLine(num1 + num2);
+            welcome d2 = (msg) => Console.WriteLine(msg);           //Lambda Function using delegates
+            welcome d3 = delegate (string msg) { Console.WriteLine(msg); }; //Anonymous Function
+
+            //Console.WriteLine(d1(2,3));
+            // d1(2, 3);
+            var ans = (string msg) => Console.Write(msg);         //Lambda Function without using delegate
+            ans("This is from lambda function without using delegate");
+            var sum = (int num1, int num2) => num1 + num2;
+            sum += (int num1, int num2) => num1 - num2;
+            Console.WriteLine(sum(6,5));
+            d2("welcome to presidio from d2");
+            d3("welcome to presidio from d3");
+        }
+    }
+}
---
+++ studentRecord.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_experiment
+{
+    public class studentRecord
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string dept { get; set; }
+        public int marks { get; set; }
+
+    }
+    public class lambda
+    {
+        public void sortedList()
+        {
+            List<studentRecord> students = new List<studentRecord>();
+            students.Add(new studentRecord() { id = 1, name = "adam", age = 22, dept = "CSE", marks = 85 });
+            students.Add(new studentRecord() { id = 2, name = "thas", age = 19, dept = "ECE", marks = 90 });
+            students.Add(new studentRecord() { id = 3, name = "vignesh", age = 17, dept = "CSE", marks = 85 });
+            students.Add(new studentRecord() { id = 4, name = "prabu", age = 21, dept = "EEE", marks = 95 });
+
+            foreach(var student in students)
+            {
+                Console.WriteLine(($"{student.id} Name: {student.name}, Age:{student.age}, Dept:{student.dept}, Score:{student.marks}"));
+            }
+            Console.WriteLine();
+            List<studentRecord> newStudentList = students.FindAll(stud => (stud.age >= 20)).ToList();
+
+            //students.Sort((s1, s2) => s1.age - s2.age);
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.name} {student.age}");
+            }
+            foreach (var student in newStudentList)
+            {
+                Console.WriteLine(($"{student.id} Name: {student.name},Age:{student.age}, Dept:{student.dept}, Score:{student.marks}"));
+            }
+        }
+    }
+}
