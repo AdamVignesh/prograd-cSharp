d34220ab1fe4fea918876c861feab04a55b24323
AdamVignesh <69408077+AdamVignesh@users.noreply.github.com>
2023-03-28 22:15:20 +0530

    RotateMatrix-code

---
+++ RotateMatrix.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace RotateMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = Convert.ToInt32(Console.ReadLine());
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[rows, cols];
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0;j<cols;j++)
+                {
+                    matrix[i,j] = Convert.ToInt32(Console.ReadLine());  
+                }
+            }
+            List<int> MyList = new List<int>();
+            for (int j = 0; j<cols; j++)
+            {
+                for (int i = rows-1; i >=0; i--)
+                {
+                    MyList.Add(matrix[i, j]);
+                }
+            }
+
+            foreach (int i in MyList)
+            {
+                Console.Write(i+" ");
+            }
+
+
+        }
+    }
+}
