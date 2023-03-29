ad5282426f09a8a7bdde6bd37f780094c441c2c7
AdamVignesh <69408077+AdamVignesh@users.noreply.github.com>
2023-03-29 19:55:35 +0530

    Actions,Func,predicates

---
+++ builtInDelegates.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace builtInDelegates
+{
+
+    delegate void Display();
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display d = helloWorld;
+            //d();
+
+            Action<string> act = (msg) => Console.WriteLine(msg);      //Action delegate
+            act("from action delegate");
+
+            Func<int, int, int> fun = (a, b) => a + b;      //FUNC delegate
+            Console.WriteLine(fun(2, 3));
+
+            Predicate<int> p = (a) => {return a > 5; };     //Predicate delegate
+            Console.WriteLine(p(7));
+        }
+
+        static void helloWorld() 
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
