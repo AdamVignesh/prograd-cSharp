ebea82566c78d5be232446c5a3d59e7830acbd52
AdamVignesh <69408077+AdamVignesh@users.noreply.github.com>
2023-03-30 23:36:33 +0530

    Unit testing in visual studio

---
+++ Calc_test.cs
@@ -0,0 +1,15 @@
+﻿namespace calculator_test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(mul(2, 3));
+        }
+
+        public static int mul(int x,int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calculator_test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace calculator_test.Tests
+{
+    [TestFixture()]
+    public class ProgramTests
+    {
+        [TestCase(2,3)]
+        [TestCase(3,3)]
+        public void mulTest(int x,int y)
+        {
+            int res = Program.mul(x,y);
+            NUnit.Framework.Assert.AreEqual(6,res);
+        }
+    }
+}
