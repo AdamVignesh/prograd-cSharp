4162d45ccee21053ae2209eb2baf7f2bdb74f2a8
AdamVignesh <69408077+AdamVignesh@users.noreply.github.com>
2023-03-31 16:48:51 +0530

    Assessment2- Time to get new license

---
+++ NewDriver'sLicense.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic;
+
+namespace NewDriver_sLicense
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter my Name");
+            string myName = Console.ReadLine();
+            Console.WriteLine("Enter the number of available agents");
+            int availableAgents = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the other four names with space");
+            string fourNames = Console.ReadLine();
+
+            string[] namesArray = fourNames.Split();
+            List<string> names = new List<string>();
+            names.Add(myName);
+            foreach(string name in namesArray)
+            {
+                names.Add(name);
+            }
+
+            List<string> sortedNameList= names.OrderBy(n=>n).ToList();
+            int batchCount = 0;
+            
+            string res = "";
+
+
+            int nameIteration = 0;
+                for (nameIteration = availableAgents * batchCount; nameIteration < availableAgents * batchCount + (availableAgents); nameIteration = nameIteration+availableAgents)
+                {
+                    if (nameIteration >= sortedNameList.Count() || sortedNameList[nameIteration]==myName)
+                    {
+                        break;
+                    }
+                   
+                }
+                
+
+            Console.WriteLine($"I''ll get my license at{nameIteration * 20}");
+        }
+    }
+}
