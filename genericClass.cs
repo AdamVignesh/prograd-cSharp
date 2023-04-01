5ba85cd173408b88ec85eaea908324a9c6dea7ea
AdamVignesh <69408077+AdamVignesh@users.noreply.github.com>
2023-04-01 11:41:16 +0530

    Generic Methods and class

---
+++ genericClass.cs
@@ -0,0 +1,23 @@
+﻿using generics;
+
+namespace generics
+{
+     internal class test<T>
+    {
+        public test(T message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+    class head
+    {
+        public static void Main()
+        {
+            test<string> gc = new test<string>("something");
+            test<int> gc1 = new test<int>(150);
+            test<float> gc2 = new test<float>(20.44f);
+        }
+
+    }
+}
